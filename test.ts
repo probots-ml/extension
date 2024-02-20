baf384d1474d01f5531cd7652a628c638df17b7b
probots-ml <87763103+probots-ml@users.noreply.github.com>
2024-02-20 15:16:38 -0300

    plotAt code bloque.

--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+/**
+ * Test screenMagic.plotAt()
+ */
+
+for(let i = 0; i < 25; i++){
+    screenMagic.plotAt(i);
+    basic.pause(200);
+}
+
+led.fadeOut(500);
+basic.pause(100);
